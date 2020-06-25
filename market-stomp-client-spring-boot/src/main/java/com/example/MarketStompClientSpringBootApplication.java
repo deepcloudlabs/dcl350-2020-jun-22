3558335fc11c912f142c1f3418fd1af0ec19b3ba
deepcloudlabs <deepcloudlabs@gmail.com>
2020-06-25 11:30:27 +0300

    initial commit

---
+++ market-stomp-client-spring-boot/src/main/java/com/example/MarketStompClientSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketStompClientSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketStompClientSpringBootApplication.class, args);
+	}
+
+}
