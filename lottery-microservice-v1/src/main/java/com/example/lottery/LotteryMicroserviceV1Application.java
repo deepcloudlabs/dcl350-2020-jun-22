92b869405152b6e759619ea158c62296159ad2a3
deepcloudlabs <deepcloudlabs@gmail.com>
2020-06-29 02:51:58 +0300

    initial commit

---
+++ lottery-microservice-v1/src/main/java/com/example/lottery/LotteryMicroserviceV1Application.java
@@ -0,0 +1,13 @@
+package com.example.lottery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotteryMicroserviceV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LotteryMicroserviceV1Application.class, args);
+	}
+
+}
---
+++ lottery-microservice-v2/src/main/java/com/example/lottery/LotteryMicroserviceV2Application.java
@@ -0,0 +1,13 @@
+package com.example.lottery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotteryMicroserviceV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LotteryMicroserviceV2Application.class, args);
+	}
+
+}
